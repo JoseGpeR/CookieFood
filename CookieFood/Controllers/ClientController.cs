36804ad66e6ae6584bdc423b3b4146165677854e
Jose Guadalupe Reyna <jgreyna52@gmail.com>
2020-03-04 18:14:33 -0600

    Ejemplo principal

--- CookieFood/Controllers/ClientController.cs
+++ CookieFood/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CookieFood.Models.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace CookieFood.Controllers
 {
     public class ClientController : Controller
     {
-        // GET: Client
+        CookieFoodEntities db = new CookieFoodEntities();
         public ActionResult ClientView()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult Menu()
+        {
+            var menu = db.CAT_PRODUCTS.ToList();
+            return Json(menu, JsonRequestBehavior.AllowGet);
         }
     }
 }
